6d9a5eafc1d8c7efb05897584b9f7c61083e5554
drutika <92363185+drutika@users.noreply.github.com>
2022-09-27 15:10:51 +0530

    Added GetInsurances Test

---
+++ EInsurance.xUnitTestProject/DbContextMocker.cs
@@ -0,0 +1,71 @@
+﻿using EInsurance.Data;
+using EInsurance.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EInsurance.xUnitTestProject
+{
+    public static class DbContextMocker
+    {
+        public static ApplicationDbContext GetApplicationDbContext(string dbName)
+        {
+            // Create a fresh service provider for the InMemory Database instance
+            var serviceProvider = new ServiceCollection()
+                                  .AddEntityFrameworkInMemoryDatabase()
+                                  .BuildServiceProvider();
+
+            // Create a new options instance telling the context
+            // to use InMemory database with the new service provider created above
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                           .UseInMemoryDatabase(dbName)
+                           .UseInternalServiceProvider(serviceProvider)
+                           .Options;
+
+            // Create the instance of the DbContext
+            var dbContext = new ApplicationDbContext(options);
+
+            // Add entities to the InMemory Database to seed the test data
+            dbContext.SeedData();
+
+            return dbContext;
+        }
+
+        // NOTE: InMemory Databases do not support Relationships, and Database Constraints properly.
+        //       So, seed the Identity Column Values also.
+        internal static readonly Insurance[] TestData_Insurances
+            = {
+                new Insurance
+                {
+                    InsuranceId = 1,
+                    InsuranceName = "First Category"
+                },
+                new Insurance
+                {
+                    InsuranceId = 2,
+                    InsuranceName = "Second Category"
+                },
+                new Insurance
+                {
+                     InsuranceId = 3,
+                    InsuranceName = "Third Category"
+                },
+            };
+
+
+        /// <summary>
+        ///     An extension Method for the DbContext object to Seed the Test Data.
+        /// </summary>
+        /// <param name="context">Application Db Context object.</param>
+        private static void SeedData(this ApplicationDbContext context)
+        {
+            context.Insurances.AddRange(TestData_Insurances);
+
+            // Commit the Changes to the database
+            context.SaveChanges();
+        }
+
+    }
+}
--- EInsurance/Controllers/InsurancesController.cs
+++ EInsurance/Controllers/InsurancesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using EInsurance.Data;
 using EInsurance.Models;
+using Microsoft.Extensions.Logging;
 
 namespace EInsurance.Controllers
 {
@@ -15,25 +16,63 @@ namespace EInsurance.Controllers
     public class InsurancesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly ILogger<InsurancesController> _logger;
 
-        public InsurancesController(ApplicationDbContext context)
+        public InsurancesController(ApplicationDbContext context, ILogger<InsurancesController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         // GET: api/Insurances
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Insurance>>> GetInsurances()
+        //public async Task<ActionResult<IEnumerable<Insurance>>> GetInsurances()
+        public async Task<IActionResult> GetInsurances()
         {
-            return await _context.Insurances
+            try
+            {
+                var insurances = await _context.Insurances
                 .Include(c => c.InsurancePolicy)
                 .ToListAsync();
+
+                // Check if data exists in the Database
+                if (insurances == null)
+                {
+                    return NotFound();          // RETURN: No data was found            HTTP 404
+                }
+                return Ok(insurances);          // RETURN: OkObjectResult - good result HTTP 200
+            }
+            catch (Exception exp)
+            {
+                return BadRequest(exp.Message); // RETURN: BadResult                    HTTP 400
+            }
         }
 
         // GET: api/Insurances/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Insurance>> GetInsurance(int id)
         {
+            //if (!id.HasValue)
+            //{
+            //    return BadRequest();
+            //}
+
+            //try
+            //{
+            //    var insurance = await _context.Insurances.FindAsync(id);
+
+            //    if (insurance == null)
+            //    {
+            //        return NotFound();
+            //    }
+
+            //    return Ok(insurance);
+            //}
+            //catch (Exception exp)
+            //{
+            //    return BadRequest(exp.Message);
+            //}
+
             var insurance = await _context.Insurances.FindAsync(id);
 
             if (insurance == null)
